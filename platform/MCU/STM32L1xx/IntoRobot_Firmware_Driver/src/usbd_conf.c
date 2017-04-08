71615b74a8df1e201b20f1428e6eee9b0ac03639
lz <lz@molmc.com>
2017-04-08 18:14:41 +0800

    ant支持虚拟串口和DFU功能

--- platform/MCU/STM32L1xx/IntoRobot_Firmware_Driver/src/usbd_conf.c
+++ platform/MCU/STM32L1xx/IntoRobot_Firmware_Driver/src/usbd_conf.c
@@ -61,7 +61,8 @@ void HAL_PCD_MspInit(PCD_HandleTypeDef *hpcd)
     GPIO_InitStruct.Pin = (GPIO_PIN_11 | GPIO_PIN_12);
     //GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;//GPIO_MODE_INPUT;
     GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-    GPIO_InitStruct.Pull = GPIO_PULLUP;
+    /* GPIO_InitStruct.Pull = GPIO_PULLUP; */
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
